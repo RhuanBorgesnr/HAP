3c3eb38fa3b56eb0b0344964d795ccec55bf7a76
Rhuan Noronha <58310932+RhuanBorgesnr@users.noreply.github.com>
2022-03-04 14:27:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,30 @@
 Rhuan Borges - 2022
 
 '''
-from math import sqrt
-
-import math
-import numpy as np
 from matplotlib import pyplot as plt
+import numpy as np
 
-# Gerar dados aleatorios 
-x = np.array(range(10))*0.5
-y = np.zeros(len(x))
-for i in range(len(x)):
-    y[i] = math.sin(x[i])
+# dados a serem gerados aleatoriamente
+dados = np.random.rand(10, 1000)
 
 
-# Criando grafico
-plt.subplot(111)
-plt.plot(x,y,marker='o')
-plt.xlabel(" X")
-plt.ylabel(" Y")
-plt.title("")
-plt.subplot(111)
+# X e Y dos dados gerados
+x = dados[:, 0].reshape(dados.shape[0], 1)
+X = np.append(x, np.ones((dados.shape[0], 1)), axis=1)
+y = dados[:, 1].reshape(dados.shape[0], 1)
 
+# Calculando os parâmetros
+calc = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+
+
+# calculando os valores do eixo 
+y_linha = X.dot(calc)
+
+# Plotando os pontos de dados
+plt.scatter(x, y)
+plt.plot(x, y_linha, 'r')
+plt.title('')
+plt.xlabel('X')
+plt.ylabel('Y')
 
 plt.show()
